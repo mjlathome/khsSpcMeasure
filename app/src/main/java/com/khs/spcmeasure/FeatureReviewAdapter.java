4ca7bc141c15507ee4cb92cad0d3c7e001229552
mjlathome <lees.mlks@gmail.com>
2015-03-22 05:16:35 -0400

    After colour.  Before getView

--- app/src/main/java/com/khs/spcmeasure/FeatureReviewAdapter.java
+++ app/src/main/java/com/khs/spcmeasure/FeatureReviewAdapter.java
@@ -3,6 +3,8 @@ package com.khs.spcmeasure;
 import android.content.Context;
 import android.database.Cursor;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 
@@ -38,10 +40,21 @@ public class FeatureReviewAdapter extends SimpleCursorAdapter {
         }
     }
 
-    // sets ListView image
+    // set row colour
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+
+    // sets ListView image and row color
+    // TODO change to CursorAdpater instead; see:
+    // http://www.gustekdev.com/2013/05/custom-cursoradapter-and-why-not-use.html
     @Override
     public void setViewImage(ImageView v, String value) {
         // super.setViewImage(v, value);
+
+        // extract parent view
+        ViewGroup row = (ViewGroup) v.getParent();
 
         // extract Measurement
         Long featId = Long.parseLong(value);
@@ -53,9 +66,11 @@ public class FeatureReviewAdapter extends SimpleCursorAdapter {
         if (meas != null) {
             Log.d(TAG, "meas - isInCtrl = " + meas.isInControl());
             v.setImageResource(meas.isInControl()? R.drawable.ic_meas_in_control : R.drawable.ic_meas_out_control);
+            row.setBackgroundColor(mContext.getResources().getColor(meas.isInControl()? R.color.measInControl : R.color.measOutControl));
         } else {
             Log.d(TAG, "meas - null");
             v.setImageResource(R.drawable.ic_meas_unknown);
+            row.setBackgroundColor(mContext.getResources().getColor(android.R.color.background_light));
         }
 
     }
